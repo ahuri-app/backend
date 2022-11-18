8923528f5836adb30a35542bdf0bc1296525586f
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-18 13:50:30 +0300

    longer tokens

--- utils/TokenGenerator.go
+++ utils/TokenGenerator.go
@@ -7,7 +7,7 @@ import (
 
 func GenerateToken() string {
 	runes := []rune("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM")
-	newToken := make([]rune, 16)
+	newToken := make([]rune, 64)
 	for i := range newToken {
 		rand.Seed(time.Now().UnixNano())
 		newToken[i] = runes[rand.Intn(len(runes))]
