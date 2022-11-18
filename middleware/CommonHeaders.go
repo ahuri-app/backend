b409be876500550f840d749c5bfd9ee8d8ef5392
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-18 20:09:17 +0300

    fix more cors and headers bugs

--- middleware/CommonHeaders.go
+++ middleware/CommonHeaders.go
@@ -3,6 +3,6 @@ package middleware
 import "github.com/gin-gonic/gin"
 
 func CommonHeaders(c *gin.Context) {
-	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Header("Content-Type", "application/json")
 	c.Next()
 }
