8a457beb87816313f794012ec8ab37d92b0c5ed4
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-19 22:24:44 +0300

    add header to cors middleware

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -2,5 +2,6 @@ import { Response } from 'express';
 
 export default (_: any, res: Response, next: Function) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 };
