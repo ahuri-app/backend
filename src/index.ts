fe6f89be9695f84d45e13ed05e3df5beb30effaa
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-19 20:39:41 +0300

    kinda working index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('hi mom');
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+if (!process.env.DATABASE_FILE_PATH) {
+  console.error('DATABASE_FILE_PATH is not set in the .env!');
+  process.exit(1);
+}
+if (!process.env.PORT) {
+  console.error('PORT is not set in the .env!');
+  process.exit(1);
+}
+if (!process.env.SALT) {
+  console.error('SALT is not set in the .env!');
+  process.exit(1);
+}
+
+const server = express();
+
+server.listen(Number(process.env.PORT), () =>
+  console.log(`Server started at 0.0.0.0:${process.env.PORT}`),
+);
