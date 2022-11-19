5699a5effd9d670f8195cff27663c1cb7ed3311c
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-19 23:18:37 +0300

    used rate limit pkg instead of buggy custom rate limiter middleware

--- src/middleware/ratelimiter.ts
+++ src/middleware/ratelimiter.ts
@@ -1,19 +1,10 @@
-import { Request, Response } from 'express';
+import expressRateLimit from 'express-rate-limit';
 
 export default (requestsLimit: number, rateInSeconds: number) => {
-  const ips: any = {};
-  return (req: Request, res: Response, next: Function) => {
-    if (!ips[req.ip]) {
-      ips[req.ip] = requestsLimit - 1;
-      setInterval(() => (ips[req.ip] = requestsLimit), 1000 * rateInSeconds);
-      next();
-      return;
-    }
-    if (ips[req.ip] === 0) {
-      res.status(429).send('Too many requests, please try again later.');
-      return;
-    }
-    ips[req.ip]--;
-    next();
-  };
+  return expressRateLimit({
+    windowMs: rateInSeconds * 1000,
+    max: requestsLimit,
+    standardHeaders: true,
+    legacyHeaders: false,
+  });
 };
