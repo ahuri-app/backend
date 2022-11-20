e6f57730d1701c551d30bc045c562f6413b114ad
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-20 10:18:47 +0300

    /auth/login added

--- src/router.ts
+++ src/router.ts
@@ -3,9 +3,11 @@ import { Router } from 'express';
 import ratelimiter from './middleware/ratelimiter';
 
 import authRegisterPOST from './routes/auth/register/POST';
+import authLoginPOST from './routes/auth/login/POST';
 
 const router = Router();
 
 router.post('/auth/register', ratelimiter(20, 60), authRegisterPOST);
+router.post('/auth/login', ratelimiter(30, 60), authLoginPOST);
 
 export default router;
