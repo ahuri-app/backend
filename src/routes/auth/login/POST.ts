50a319359f4e278d3c880adf797c7825d91d45fe
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-20 10:35:49 +0300

    finished /auth/login

--- src/routes/auth/login/POST.ts
+++ src/routes/auth/login/POST.ts
@@ -1,5 +1,56 @@
 import { Request, Response } from 'express';
+import { hash, salt } from '../../../utils/crypto';
+import db from '../../../utils/db';
 
-export default (req: Request, res: Response) => {
-  res.send('hi');
+export default async (req: Request, res: Response) => {
+  try {
+    const trimmedEmail = String(req.body.email || '').trim();
+    const trimmedPassword = String(req.body.password || '').trim();
+
+    if (!trimmedEmail) {
+      res.status(400).json({
+        message: 'Email not set',
+        payload: null,
+      });
+      return;
+    }
+    if (!trimmedPassword) {
+      res.status(400).json({
+        message: 'Password not set',
+        payload: null,
+      });
+      return;
+    }
+
+    const user = await db.user.findFirst({
+      where: {
+        email: trimmedEmail,
+        password: hash(salt(trimmedPassword)),
+      },
+    });
+
+    if (!user) {
+      res.status(401).json({
+        message: 'Incorrect credentials',
+        payload: null,
+      });
+      return;
+    }
+
+    res.status(200).json({
+      message: 'Success',
+      payload: {
+        id: user.eid,
+        email: user.email,
+        username: user.username,
+        tag: user.tag,
+        token: user.token,
+      },
+    });
+  } catch {
+    res.status(500).json({
+      message: 'Internal server error',
+      payload: null,
+    });
+  }
 };
