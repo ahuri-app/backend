1a4d5f3a5175241319283595cf6b71a293fd7d06
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-21 15:52:30 +0300

    added sending messages

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,7 @@ import authLoginPOST from './routes/auth/login/POST';
 import channelPOST from './routes/channel/POST';
 import channelPARAMidGET from './routes/channel/[id]/GET';
 import channelPARAMidDELETE from './routes/channel/[id]/DELETE';
+import channelPARAMidSendMessage from './routes/channel/[id]/send-message/POST';
 import accountDELETE from './routes/account/DELETE';
 import accountGET from './routes/account/GET';
 
@@ -17,6 +18,11 @@ router.post('/auth/login', ratelimiter(30, 60), authLoginPOST);
 router.post('/channel', ratelimiter(12, 60), channelPOST);
 router.get('/channel/:id', ratelimiter(5, 1), channelPARAMidGET);
 router.delete('/channel/:id', ratelimiter(12, 60), channelPARAMidDELETE);
+router.post(
+  '/channel/:id/send-message',
+  ratelimiter(5, 1),
+  channelPARAMidSendMessage,
+);
 router.delete('/account', ratelimiter(20, 60), accountDELETE);
 router.get('/account', ratelimiter(30, 60), accountGET);
 
---
+++ src/routes/channel/[id]/send-message/POST.ts
@@ -0,0 +1,111 @@
+import { Request, Response } from 'express';
+import db from '../../../../utils/db';
+import getCurrentDateFormatted from '../../../../utils/getCurrentDateFormatted';
+
+export default async (req: Request, res: Response) => {
+  try {
+    if (!req.headers.authorization) {
+      res.status(400).json({
+        message:
+          'There is no Authorization header, please set it to a valid token',
+        payload: null,
+      });
+      return;
+    }
+
+    const user = await db.user.findFirst({
+      where: {
+        token: req.headers.authorization,
+      },
+    });
+
+    if (!user) {
+      res.status(401).json({
+        message: 'Invalid token',
+        payload: null,
+      });
+      return;
+    }
+
+    const channel = await db.channel.findFirst({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!channel) {
+      res.status(404).json({
+        message: 'Channel not found',
+        payload: null,
+      });
+      return;
+    }
+
+    const trimmedMessage = String(req.body.content || '').trim();
+
+    if (!trimmedMessage) {
+      res.status(400).json({
+        message: 'Message content is not set or empty',
+        payload: null,
+      });
+      return;
+    }
+
+    if (trimmedMessage.length > 2000) {
+      res.status(400).json({
+        message: 'Message content is over the 2000 characters limit',
+        payload: null,
+      });
+      return;
+    }
+
+    const message = await db.message.create({
+      data: {
+        content: trimmedMessage,
+        senderId: user.id,
+        channelId: channel.id,
+        sentAt: getCurrentDateFormatted(),
+      },
+      select: {
+        id: true,
+        sender: {
+          select: {
+            id: true,
+            username: true,
+            tag: true,
+            badges: true,
+            createdAt: true,
+          },
+        },
+        channel: {
+          select: {
+            id: true,
+            name: true,
+            owner: {
+              select: {
+                id: true,
+                username: true,
+                tag: true,
+                badges: true,
+                createdAt: true,
+              },
+            },
+            createdAt: true,
+          },
+        },
+        content: true,
+        sentAt: true,
+      },
+    });
+
+    res.status(200).json({
+      message: 'Sent message',
+      payload: message,
+    });
+  } catch {
+    res.status(500).json({
+      message: 'Internal server error',
+      payload: null,
+    });
+  }
+};
