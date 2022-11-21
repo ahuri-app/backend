3c6171a240a4a90c4c688a9d51fa852d8af453f2
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-21 20:38:28 +0300

    add GET /channel

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import ratelimiter from './middleware/ratelimiter';
 import authRegisterPOST from './routes/auth/register/POST';
 import authLoginPOST from './routes/auth/login/POST';
 import channelPOST from './routes/channel/POST';
+import channelGET from './routes/channel/GET';
 import channelPARAMidGET from './routes/channel/[id]/GET';
 import channelPARAMidDELETE from './routes/channel/[id]/DELETE';
 import channelPARAMidSendMessage from './routes/channel/[id]/send-message/POST';
@@ -16,6 +17,7 @@ const router = Router();
 router.post('/auth/register', ratelimiter(20, 60), authRegisterPOST);
 router.post('/auth/login', ratelimiter(30, 60), authLoginPOST);
 router.post('/channel', ratelimiter(12, 60), channelPOST);
+router.get('/channel', ratelimiter(5, 1), channelGET);
 router.get('/channel/:id', ratelimiter(5, 1), channelPARAMidGET);
 router.delete('/channel/:id', ratelimiter(12, 60), channelPARAMidDELETE);
 router.post(
---
+++ src/routes/channel/GET.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from 'express';
+import db from '../../utils/db';
+
+export default async (req: Request, res: Response) => {
+  try {
+    if (!req.headers.authorization) {
+      res.status(400).json({
+        message:
+          'There is no Authorization header, please set it to a valid token',
+        payload: null,
+      });
+      return;
+    }
+
+    const user = await db.user.findFirst({
+      where: {
+        token: req.headers.authorization,
+      },
+    });
+
+    if (!user) {
+      res.status(401).json({
+        message: 'Invalid token',
+        payload: null,
+      });
+      return;
+    }
+
+    const channels = await db.channel.findMany({
+      select: {
+        id: true,
+        name: true,
+        owner: {
+          select: {
+            id: true,
+            username: true,
+            tag: true,
+            badges: true,
+            createdAt: true,
+          },
+        },
+        createdAt: true,
+      },
+    });
+
+    res.json({
+      message: 'Success',
+      payload: channels,
+    });
+  } catch {
+    res.status(500).json({
+      message: 'Internal server error',
+      payload: null,
+    });
+  }
+};
