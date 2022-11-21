3ffef3c86de633e68022ba4b86acfb737c0accd6
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-21 11:20:23 +0300

    forgor to add the new account routes to router

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,8 @@ import authLoginPOST from './routes/auth/login/POST';
 import channelPOST from './routes/channel/POST';
 import channelPARAMidGET from './routes/channel/[id]/GET';
 import channelPARAMidDELETE from './routes/channel/[id]/DELETE';
+import accountDELETE from './routes/account/DELETE';
+import accountGET from './routes/account/GET';
 
 const router = Router();
 
@@ -15,5 +17,7 @@ router.post('/auth/login', ratelimiter(30, 60), authLoginPOST);
 router.post('/channel', ratelimiter(12, 60), channelPOST);
 router.get('/channel/:id', ratelimiter(5, 1), channelPARAMidGET);
 router.delete('/channel/:id', ratelimiter(12, 60), channelPARAMidDELETE);
+router.delete('/account', ratelimiter(20, 60), accountDELETE);
+router.get('/account', ratelimiter(30, 60), accountGET);
 
 export default router;
